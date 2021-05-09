c68f829bdb1f5f59e84c6821ca47ec7e0b2e53a5
Asadullah Kashif <s.asad18k@gmail.com>
2021-05-10 02:19:03 +0500

    added / route

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 // Routes
 const { QuestionRoute } = require("./routes/QuestionRoute");
 const { UserRoute } = require("./routes/UserRoute");
-const { VerificationRoute } = require('./routes/JWTVerificationRoute');
+const { VerificationRoute } = require("./routes/JWTVerificationRoute");
 
 const PORT = parseInt(process.env.PORT) || 5000;
 const DB_CONNECTION = process.env.DB_CONNECTION;
@@ -15,13 +15,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 // Setting up the routes
+app.get("/", (req, res) => res.send("home"));
 app.use("/api/questions", QuestionRoute);
 app.use("/auth/user", UserRoute);
 app.use("/auth/verify", VerificationRoute);
 
-app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT} ...`));
-
-mongoose.connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, (error) =>
-  console.log(!error ? "Connected to MongoDB..." : error)
+app.listen(PORT, () =>
+  console.log(`Server is running at http://localhost:${PORT} ...`)
 );
- 
+
+mongoose.connect(
+  DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error) => console.log(!error ? "Connected to MongoDB..." : error)
+);
